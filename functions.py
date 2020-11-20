8b4cd498efe4ac424dbe6da5d59c9d3c63535e61
Sasha Sinitsyn <39600558+AlexandrSinitsyn@users.noreply.github.com>
2020-11-20 19:35:56 +0300

    Homework updation

---
+++ functions.py
@@ -0,0 +1,11 @@
+def out(message: str) -> None:
+    print(message)
+
+
+def get_answer() -> bool:
+    answer = input()
+
+    if answer == 'Y':
+        return True
+    else:
+        return False
