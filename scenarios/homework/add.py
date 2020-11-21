ede5f148a00d17bbb80d72791e2df63287254311
312565 Михаил Кулешов <b04kamedy@gmail.com>
2020-11-21 14:49:07 +0300

    адд

--- scenarios/homework/add.py
+++ scenarios/homework/add.py
@@ -1,5 +1,7 @@
 from bot import bot
 from telebot import types
+from models import Homework
+from db import session
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "add homework")
@@ -11,16 +13,13 @@ def add_homework(message):
 def upload_homework(message):
     chat_id = message.chat.id
     homework_id = message.document.file_id
-    homework = Homework(task=homework_id, deadline=None)
+    homework = Homework(file_telegram_id=homework_id, deadline="")
+    session.add(homework)
+    session.commit()
+    bot.send_message(message, "Файл загружен!")
 
-    user.current_homework = homework
-
-    create_homework(homework)
-    msg = bot.send_message(chat_id, "Установите дедлайн (1.1.2020)")
-    bot.register_next_step_handler(msg, set_deadline)
-
-def set_deadline(message):
-    chat_id = message.chat.id
-    homework = user.current_homework
-    homework.deadline = message.text
-    #Ой да фпесду
+# def set_deadline(message):
+#     chat_id = message.chat.id
+#     homework = user.current_homework
+#     homework.deadline = message.text
+#     #Ой да фпесду
