57b5fe04eacea072744a15679170c9314a8274b1
312565 Михаил Кулешов <b04kamedy@gmail.com>
2020-11-21 15:30:16 +0300

    ggg

--- scenarios/homework/add.py
+++ scenarios/homework/add.py
@@ -4,7 +4,7 @@ from models import Homework
 from db import session
 
 
-@bot.callback_query_handler(func=lambda call: call.data == "add homework")
+@bot.message_handler(func=callback("Добавить дз"))
 def add_homework(message):
     chat_id = message.chat.id
 	msg = bot.send_message(chat_id, 'Загрузите файл с ДЗ')
