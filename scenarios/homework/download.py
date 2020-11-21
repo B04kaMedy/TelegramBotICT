c2fdda536d251a7547abba3aea58daca6596fb74
Semenov Roman <3908441@gmail.com>
2020-11-21 16:03:14 +0300

    asd

--- scenarios/homework/download.py
+++ scenarios/homework/download.py
@@ -2,7 +2,7 @@ from bot import bot
 from telebot import types
 from models import Homework, Group
 from db import session
-
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 from filters import callback
 
 @bot.message_handler(func=callback("Скачать домашнее задание"))
@@ -15,7 +15,7 @@ def add_homework(message):
 
     bot.send_message(chat_id, "Выберите ДЗ:", reply_markup=markup)
 
-    bot.register_next_step_handler(msg, upload_homework)
+    bot.register_next_step_handler(message, upload_homework)
 
 def upload_homework(message):
     chat_id = message.chat.id
