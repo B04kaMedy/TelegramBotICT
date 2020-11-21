85d231829f92b5482b6e062d889b732784971780
312565 Михаил Кулешов <b04kamedy@gmail.com>
2020-11-21 17:06:54 +0300

    fffff

---
+++ scenarios/homework/check.py
@@ -0,0 +1,63 @@
+from bot import bot
+from telebot import types
+from models import Homework, Group, CompletedHomework, User
+from db import session
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from filters import callback
+from scenarios.main_menu import send_main_menu
+
+@bot.message_handler(func=callback("Проверить дз"))
+def check_homework(message):
+    chat_id = message.chat.id
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True)
+
+    for hw in message.current_group.homeworks:
+        markup.add(KeyboardButton(hw.id))
+
+    bot.send_message(chat_id, "Выберите дз для проверки", reply_markup=markup)
+
+    bot.register_next_step_handler(message, check_next)
+
+states = dict()
+
+def check_next(message):
+    chat_id = message.chat.id
+    homework = session.query(Homework).get(message.text)
+
+    states[chat_id] = homework
+
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True)
+
+    for completed_homework in homework.completed_homeworks():
+        if completed_homework.is_check():
+            continue
+        markup.add(KeyboardButton(completed_homework.student.name))
+    
+    bot.send_message(chat_id, "ВЫберите ученика", reply_markup=markup)
+    bot.register_next_step_handler(message, check_next2)
+
+def check_next2(message)
+    chat_id = message.chat.id
+    homework = states[chat_id]
+    
+    username = message.text
+    user = session.query(User).filter_by(name=username)
+    completed_homework = session.query(CompletedHomework).filter_by(student=user, homework=homework).one()
+    states[chat_id] = completed_homework
+
+    bot.send_document(message.chat.id, completed_homework.file_telegram_id)
+
+    bot.send_message(chat_id, "Оценка за дз:")
+    bot.register_next_step_handler(message, check_end)
+
+def check_end(message):
+    chat_id = message.chat.id
+    completed_homework = states[chat_id]
+    completed_homework.mark = int(message.text)
+
+    session.commit()
+
+    bot.send_message(chat_id, "Проверено")
+    send_main_menu(message)
+
+
