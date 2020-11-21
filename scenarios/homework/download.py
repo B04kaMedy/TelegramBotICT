6872001bc8a5b518a1547a8e3f21f3fbd4ce0b6f
312565 Михаил Кулешов <b04kamedy@gmail.com>
2020-11-21 15:57:30 +0300

    ggggg :)

--- scenarios/homework/download.py
+++ scenarios/homework/download.py
@@ -1,6 +1,6 @@
 from bot import bot
 from telebot import types
-from models import Homework
+from models import Homework, Group
 from db import session
 
 from filters import callback
@@ -8,5 +8,19 @@ from filters import callback
 @bot.message_handler(func=callback("Скачать домашнее задание"))
 def add_homework(message):
     chat_id = message.chat.id
-    msg = bot.send_message(chat_id, 'Загрузите файл с ДЗ')
+    markup = ReplyKeyboardMarkup(one_time_keyboard=True)
+
+    for hw in message.current_group.homeworks:
+        markup.add(KeyboardButton(hw.id))
+
+    bot.send_message(chat_id, "Выберите ДЗ:", reply_markup=markup)
+
     bot.register_next_step_handler(msg, upload_homework)
+
+def upload_homework(message):
+    chat_id = message.chat.id
+    homework = session.query(Homework).get(message.text)
+
+    bot.send_document(message.chat.id, homework.file_telegram_id)
+
+    send_main_menu(message)
